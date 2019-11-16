17850e96dca4e2cf8a29dcf53c81f0934205a875
Thu Nguyen <thu.nguyen@pi.exchange>
2019-11-17 01:49:13 +1100

    Add 2 cache - longterm cache - for serving stale data, shorterm for performance

--- server/api/controllers/weather/controller.js
+++ server/api/controllers/weather/controller.js
@@ -1,14 +1,29 @@
-import WeatherService from '../../services/weather.service'
+import WeatherService from "../../services/weather.service";
+import { Cache } from "memory-cache";
+import l from "../../../common/logger";
+
+const cache = new Cache();
 
 class Controller {
-
   weatherByCity(req, res) {
-    WeatherService.weatherByCity(req.params.city).then(r => {
-      if (r)
-        res.json(r);
-      else   
-        res.status(404).end();
-    });  
+    
+    const city = req.params.city;
+    const cached = cache.get(city);
+
+    if (cached) {
+      res.json(cache.get(city));
+    } else {
+      WeatherService.weatherByCity(req.params.city).then(r => {
+        if (r) {
+          try {
+            cache.put(city, r, parseInt(process.env.CACHED_EXPIRE_IN));
+          } catch (err) {
+            l.error(`caching failed. ${err.message}`);
+          }
+          res.json(r);
+        } else res.status(404).end();
+      });
+    }
   }
 }
 
